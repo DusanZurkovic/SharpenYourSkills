3c5fe00e6fe9fe598c14e45ddaa41683cf12bd50
Dusan Zurkovic <d.zurkovic@vega.local>
2021-11-21 16:10:54 +0100

    first commit

---
+++ SharpenYourSkills/Model/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SharpenYourSkills.Model
+{
+    public class Product
+    {
+        public String name { get; set; }
+        public int upc { get; set; }
+        public double price { get; set; }
+
+        public Product(String name, int upc, double price) 
+        {
+            this.name = name;
+            this.upc = upc;
+            this.price = price + (price * 1 / 5);
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + name + " UPC: "+ upc + " Price: " + price;
+        }
+    }
+}
